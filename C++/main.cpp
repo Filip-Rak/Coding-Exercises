5728de0d5a2ebbbcaaf43da0b37f21734b196e53
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-03-25 14:19:48 +0100

    add solution

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <vector>
+
+/* Sales by Match */
+// There is a large pile of socks that must be paired by color. 
+// Given an array of integers representing the color of each sock, 
+// determine how many pairs of socks with matching colors there are
+// 1 <= n <= 100
+
+int sockMerchant(int n, std::vector<int> ar)
+{
+    // Store all occurances
+    const int MAX_ID = 100;
+    std::vector<int> freq(MAX_ID, 0);
+
+    // Map all numbers to vector
+    for (int i = 0; i < n; i++)
+    {
+        freq[ar[i] - 1] += 1;
+    }
+
+    // Count all pairs
+    int count = 0;
+    for (int f : freq)
+    {
+        count += f / 2; // Divide as integers
+    }
+
+    return count;
+}
 
 int main()
 {
