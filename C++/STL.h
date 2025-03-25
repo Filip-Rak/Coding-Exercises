44700a074b42d7350fa809ce4e9f40d86075a5cc
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-03-25 17:48:09 +0100

    add exercise

--- C++/STL.h
+++ C++/STL.h
@@ -24,3 +24,41 @@ int load_and_sort()
     for (int num : numbers)
         std::cout << num << " ";
 }
+
+/*
+ * You are given a vector of N integers.
+ *
+ * 1. Perform a query that takes a single integer representing a position in the vector.
+ *    Erase the element at that position.
+ *
+ * 2. Perform another query that takes two integers representing a range [start, end).
+ *    Erase all elements in the vector that fall within this range.
+ *
+ * Note: The second query should be performed on the vector after completing the first query.
+ */
+int load_and_erase()
+{
+    int size;
+    std::cin >> size;
+
+    std::vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+        std::cin >> arr[i];
+
+    int q1;
+    std::cin >> q1;
+
+    int q2_1;
+    std::cin >> q2_1;
+
+    int q2_2;
+    std::cin >> q2_2;
+
+    arr.erase(arr.begin() + q1 - 1);
+    arr.erase(arr.begin() + q2_1 - 1, arr.begin() + q2_2 - 1);
+
+    std::cout << arr.size() << "\n";
+
+    for (int num : arr)
+        std::cout << num << " ";
+}
