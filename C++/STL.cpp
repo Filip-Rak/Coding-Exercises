e5a29fa9efda278839eaac8d45c3e82b0fec951a
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-04-06 23:23:44 +0200

    fix comment

--- C++/STL.cpp
+++ C++/STL.cpp
@@ -833,23 +833,6 @@ void variadic_bits_entry()
     std::cout << sum(5, 3, 2, 10) << "\n";
 }
 
-/*
-You are given four integers: N, S, P, Q. You will use them in order to create
-the sequence a with the following pseudo-code:
-
-    a[0] = S (modulo 2^31)
-    for i = 1 to N - 1
-        a[i] = a[i - 1] * P + Q (modulo 2^31)
-
-Your task is to calculate the number of distinct integers in the sequence a.
-
-Input Format:
-Four space-separated integers on a single line: N, S, P, and Q respectively.
-
-Output Format:
-A single integer that denotes the number of distinct integers in the sequence a.
-*/
-
 void set_usage1()
 {
     // Load input
