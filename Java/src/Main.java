a769c552f93894c767a9efc6e00bae9aabb3c6d7
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-04-12 17:24:57 +0200

    add exercise

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        SimpleExercises.entry_simple_input();
+        SimpleExercises.loops_entry();
     }
 }
--- Java/src/SimpleExercises.java
+++ Java/src/SimpleExercises.java
@@ -2,19 +2,60 @@ import java.util.Scanner;
 
 public class SimpleExercises
 {
-    public static void entry_simple_input(){
+    public static void entry_simple_input()
+    {
         Scanner scanner = new Scanner(System.in);
 
         while(scanner.hasNext())
         {
-            try{
+            try
+            {
                 int buffer = scanner.nextInt();
                 System.out.println(buffer);
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 System.out.println("Only numbers are welcome! " + e);
                 scanner.next();
             }
+        }
+    }
+
+    public static void loops_entry()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int q = scan.nextInt();
+        while (q > 0)
+        {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int n = scan.nextInt();
+
+            print_query(a, b, n);
+            System.out.print("\n");
+
+            q -= 1;
+        }
+    }
+    /**
+     * Generates a mathematical series for each query based on the given parameters.
+     * <p>
+     * The series is constructed using the formula:
+     * (a + 2^0 * b), (a + 2^0 * b + 2^1 * b), ..., (a + 2^0 * b + 2^1 * b + ... + 2^(n-1) * b)
+     * <p>
+     * For each query containing values of `a`, `b`, and `n`, this function computes and prints
+     * a line of `n` space-separated integers representing the series.
+     */
+    private static void print_query(int a, int b, int n)
+    {
+        int sum = a + b;
+        System.out.print(sum + " ");
+
+        for (int i = 1; i < n; i++)
+        {
+            sum += (int)(Math.pow(2, i) * b);
+            System.out.print(sum + " ");
         }
     }
 }
