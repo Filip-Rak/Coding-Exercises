ad7f413de21bcb818fb3709ba23ec28dd466ba28
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-04-14 18:42:44 +0200

    add helper pair class

--- Java/src/SimpleExercises.java
+++ Java/src/SimpleExercises.java
@@ -180,4 +180,18 @@ public class SimpleExercises
             System.out.print(num + " ");
         }
     }
+
+    class Pair<T, U>
+    {
+        /* Attributes */
+        T first;
+        U second;
+
+        /* Constructor */
+        Pair(T first, U second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }
 }
